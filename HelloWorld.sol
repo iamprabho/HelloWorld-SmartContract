7b17b30a795352e12569dbf81ac9c0040ff3503f
prathamesh <91321244+iamprabho@users.noreply.github.com>
2022-05-01 22:54:56 +0530

    Add files via upload

---
+++ HelloWorld.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity >=0.7.3;
+contract HelloWorld{
+    event messagechanged(string oldmsg,string newmsg);
+    string public message;
+    constructor(string memory firstmessage){
+        message = firstmessage;
+
+    }
+    function update(string memory newmessage)public {
+        string memory oldmsg = message;
+        message=newmessage;
+        emit messagechanged(oldmsg, newmessage);
+        
+
+    }
+
+}
